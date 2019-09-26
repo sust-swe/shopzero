6886e3635091fd28c392dc9faafd8c2215923715
Ahsan Aziz Ishan <ishan.bn14@gmail.com>
2019-09-26 15:09:13 +0600

    Created Signup for Users

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,7 @@ Rails.application.routes.draw do
   scope 'admin' do
   	resources :admin
   end
-<<<<<<< Updated upstream
   scope 'users' do
     post '/signup', to: 'users#create'
   end
-=======
-
-  scope 'users' do
-    resources :users
-  end
-
->>>>>>> Stashed changes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2019_09_25_091318) do
-=======
-ActiveRecord::Schema.define(version: 2019_09_24_075316) do
->>>>>>> Stashed changes
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
