576fa32f11433e966b64af0393b115bfbabf703c
Tushita2016831026 <kazi26@student.sust.edu>
2019-11-09 14:59:51 +0600

    CORS update

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Shopzero
      config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options, :delete]
       end
     end
   end
