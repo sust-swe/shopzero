3311cecefb78cc6cfb4508a0346e878bcdcdb1c7
Ahsan Aziz Ishan <ahsan17@student.sust.edu>
2019-11-13 14:51:45 +0600

    Remove Items from Cart On Checkout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,19 @@
 class OrdersController < ApplicationController
+  include CartItemsHelper
 
   skip_before_action :verify_authenticity_token
   before_action :authorize_request
 
   def create
-    @cart_items = current_user.cart_items.where("checked_at_checkout like ?",true)
+    @cart_items = current_user.cart_items.where("checked_at_checkout like ?", true)
     order_no = Order.new.new_order_no
     order_messages = []
     @cart_items.each do |cart_item|
       order = current_user.orders.new(order_no: order_no, product_id: cart_item.product_id)
-      order_messages.push(message: "#{cart_item.product.name} #{order.save ? "success" : "failed"}") 
+      order_messages.push(message: "#{cart_item.product.name} #{order.save ? "success" : "failed"}")
+      cart_item.delete
     end
     render json: order_messages.as_json
+    broadcast_cart
   end
 end
--- app/helpers/cart_items_helper.rb
+++ app/helpers/cart_items_helper.rb
@@ -1,5 +1,10 @@
 module CartItemsHelper
   def cart_json_params
-    [:id,:user_id,:product_id,:count,:checked_at_checkout]    
+    [:id, :user_id, :product_id, :count, :checked_at_checkout]
+  end
+
+  def broadcast_cart
+    @cart_items = current_user.cart_items
+    CartItemsChannel.broadcast_to(current_user, @cart_items.as_json) and return
   end
 end
