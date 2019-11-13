0e98f018c073ebcc87cc1be2dbde05047e98d54e
Ahsan Aziz Ishan <ahsan17@student.sust.edu>
2019-11-13 15:01:04 +0600

    Remove ambiguity

--- app/channels/cart_items_channel.rb
+++ app/channels/cart_items_channel.rb
@@ -1,7 +1,7 @@
 class CartItemsChannel < ApplicationCable::Channel
   def subscribed
     stream_for current_user
-    CartItemsChannel.broadcast_to(self.current_user, self.current_user.cart_items.as_json)
+    CartItemsChannel.broadcast_to(current_user, current_user.cart_items.as_json)
   end
 
   def unsubscribed
