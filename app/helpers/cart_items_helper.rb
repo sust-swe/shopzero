b26bb69d90c4c8348e244f240a7fb1e75f87cf32
Ishan (2016831017) <ahsan17@student.sust.edu>
2019-12-05 21:46:01 +0600

    Bugfix: Broadcast product includes brand name now

--- app/helpers/cart_items_helper.rb
+++ app/helpers/cart_items_helper.rb
@@ -12,7 +12,7 @@ module CartItemsHelper
   end
 
   def broadcast_cart_params(cart_items)
-    cart_items.as_json(include: { product: { only: product_json_params } },
+    cart_items.as_json(include: { product: { only: product_json_params, include: [:brand] } },
                        only: cart_json_params)
   end
 end
